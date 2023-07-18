ce46804c2ab25a5fa8898087c1493f874adfb98d
Gawain Antell <46300365+GwenAntell@users.noreply.github.com>
2023-07-18 13:33:41 -0700

    update study vars; output subsample correlations

--- R/timeseries_corr_comparison_sans_subsampling.R
+++ R/timeseries_corr_comparison_sans_subsampling.R
@@ -1,6 +1,11 @@
-library(sf)
-library(units)
-library(vegan)
+library(divvy) # uniqify and sdsumry functions
+# for stationarity and cross-correlation tests on time series
+library(fUnitRoots) 
+library(TSA)
+# table and plot outputs
+library(stringr)
+library(xtable)
+options(xtable.timestamp = "")
 
 # Prep subsample data -----------------------------------------------------
 
@@ -17,48 +22,111 @@ subsmpl <- subsmpl[!is.na(subsmpl$mst_med),]
 globlOccFl <- 'Data/DS1_unique_taxonLocationTime_occurrences.csv'
 globlOcc <- read.csv(globlOccFl)
 
+coordCols <- c('centroid_long', 'centroid_lat')
+prj <- 'ESRI:54010' # Eckhert VI
+
 # calculate study variables over all occurrences in each time bin
+globlOcc <- uniqify(globlOcc, taxVar = 'unique_name', xy = coordCols)
 
-# square-root minimum spanning tree length for each species
-calcMst <- function(x, dat, nameCol, coordCols, prj){
-  rows <- which(dat[, nameCol] == x)
-  if (length(rows) < 2) { 
-    return(NA) 
-  } else {
-    coordSf <- st_as_sf(dat[rows,], coords = coordCols, crs = prj)
-    gcdists <- st_distance(coordSf) |> set_units(value = 'km')
-    tr <- spantree(drop_units(gcdists))
-    sum(tr$dist) |> sqrt()
-  }
-} 
+globlList <- sapply(bins, function(b){
+  binBool <- globlOcc$stage == b
+  sdsumry(globlOcc[binBool,], taxVar = 'unique_name', 
+          xy = coordCols, crs = prj)
+  }, simplify = FALSE)
+globlMeta <- do.call(rbind, globlList) |> data.frame()
 
-# return study variables in one time bin
-calcVars <- function(bin, nameCol, coordCols, prj){
+# mean proportional occupancy without singletons, all taxa in one time bin
+getPropOcc <- function(bin, nameCol, coordCols, prj){
   binBool <- globlOcc$stage == bin
   binDat <- globlOcc[binBool, ]
-  # total species count
-  binSpp <- unique(binDat[, nameCol])
-  rich <- length(binSpp)
-  
-  # mean occupancy without singletons
+  nSite <- unique(binDat$cell_number) |> length()
   occSpp <- table(binDat[, nameCol]) |> as.numeric()
-  occMean <- occSpp[occSpp > 1] |> mean()
-  
-  # median square-root MST
-  mstSpp <- sapply(binSpp, calcMst, dat = binDat, nameCol = nameCol, 
-                   coordCols = coordCols, prj = prj)
-  mstMed <- median(mstSpp, na.rm = TRUE)
-  
-  cbind('species_count' = rich,
-             'oc_wo.singl_mean' = occMean, 
-             'mst_med' = mstMed)
+  occProp <- occSpp[occSpp > 1] / nSite
+  mean(occProp)
 }
-
 # iterate over bins
-globlList <- sapply(bins, calcVars, nameCol = 'unique_name', 
-                coordCols = c('centroid_long', 'centroid_lat'),
-                prj = 'ESRI:54010', # Eckhert VI
-                simplify = FALSE # retain column names in output
-                ) 
-globlMeta <- do.call(rbind, globlList) |> data.frame()
+globlMeta$propOcc <- sapply(bins, getPropOcc, nameCol = 'unique_name', 
+                            coordCols = coordCols, prj = prj
+                            ) 
 globlMeta$bin <- names(globlList)
+
+# Subsample correlations table --------------------------------------------
+
+# determine the number of successful bootstrap replications
+n <- sapply(bins, function(t) sum(subsmpl$bin==t))
+nruns <- min(n)
+
+resp_vars <- c('oc_wo.singl_mean', 'species_count') # 'mst_med'
+pred_vars <- c('species_count','mst_sample') # 'pool_n'
+
+cors <- matrix(nrow = length(resp_vars), ncol = length(pred_vars),
+               dimnames = list(resp_vars, pred_vars))
+
+# extract the i-th subsample from every time step
+get_seq <- function(t, iter, cols, dat){
+  series_rows <- which(dat$bin == t)[iter]
+  dat[series_rows, cols]
+} 
+
+# Non-parametric correlation for a single time series at a time
+# This function will run in parallel across bootstrap replicates,
+# within a loop across time bins
+get_cor <- function(pred, r_var, dat, iter){ 
+  cols <- c(pred, r_var)
+  series_list <- lapply(bins, get_seq, iter=iter, cols=cols, dat=dat)
+  series <- matrix(unlist(series_list), ncol=length(cols), 
+                   dimnames=list(bins, cols), byrow=TRUE)
+  
+  # fit AR1 model to the predictor, non-stationary series
+  AR <- arima(series[,pred], order=c(1, 0, 0)) 
+  series[,pred] <- as.numeric(AR$residuals) 
+  
+  # return only the point estimate of the coefficient
+  # error bars come from bootstrapping, so no need to save significance values here
+  cor(series[,c(r_var,pred)], method='kend')[1,2] 
+} 
+
+for (k in 1:length(pred_vars)){
+  pred <- pred_vars[k]
+  
+  for (i in 1:length(resp_vars)){ 
+    r_var <- resp_vars[i]
+    
+    # calculate across all iterations
+    cors_i <- sapply(1:nruns, get_cor, pred = pred, r_var = r_var, dat = subsmpl)
+    
+    # 95% CI for 2-sided tests
+    cors_avg <- mean(cors_i, na.rm = TRUE)
+    cors_CI <- quantile(cors_i, probs=c(0.025, 0.975), na.rm=TRUE)
+    
+    # round (preserve trailing zeroes) and combine into 1 string
+    roundSpcl <- function(x){
+      sprintf("%.2f", round(x, digits=2))
+    }
+    rounded <- sapply(c(cors_avg, cors_CI), roundSpcl) 
+    cors[i,k] <- paste(rounded[1], '_(', paste(rounded[2:3], collapse=', '), ')', sep='')
+  } # end loop through range metrics
+} # loop through each predictor variable
+
+# format and save output as LaTeX table
+
+sp_count_cors <- unlist(str_split(cors[,'species_count'], '_'))
+sp_count_m <- matrix(sp_count_cors, ncol=2, byrow=TRUE)
+agg_cors <- unlist(str_split(cors[,'mst_sample'], '_'))
+agg_m <- matrix(agg_cors, ncol=2, byrow=TRUE)
+fancy_cors <- cbind(resp_vars, sp_count_m, agg_m)
+
+tabl <- xtable(fancy_cors, type = "latex", align=c('l','l','r','c','r','c') )  
+caption(tabl) <- "Correlation between subsampled time series"
+addtorow <- list()
+addtorow$pos <- list(0, 0, 0)
+addtorow$command <- c("& \\multicolumn{4}{c}{Correlation with predictor time series} \\\\ \\cline{2-5} \n",
+                      "& \\multicolumn{2}{c}{Species count} & \\multicolumn{2}{c}{Sampling aggregation} \\\\\n",
+                      "Response series & Mean & 95\\% & Mean & 95\\% \\\\\n")
+day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%Y-%m-%d') 
+name_rich <- paste0('tau_range_vs_richness_subsampled_', day, '.tex')
+noDot <- function(str){ gsub("_", " ", str, fixed = TRUE) }
+print(tabl, add.to.row = addtorow, file = name_rich, 
+      caption.placement = 'top', booktabs = TRUE,
+      include.colnames = FALSE, include.rownames = FALSE, 
+      sanitize.text.function = noDot)
